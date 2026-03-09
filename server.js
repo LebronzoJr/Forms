99c904401c78851aef1f14ce1ca478aee20a94eb
Lebron <alebron1290@gmail.com>
2026-03-09 11:54:03 -0400

    CompletedAdderPractice

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const app = express();
 app.set ('view enjine', 'ejs');
 app.use(express.static("public"));
 
-app.get('/submit')
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.listen(300);
+app.get('/solution', (req,res) =>{
+    let num1 = parseInt(req.query['num1']);
+    let num2 = parseInt(req.query['num2']);
+    let total =num1 + num2;
+    res.send(`<h2>${num} + ${num2} = ${total} </h2>`);
+});
+
+//GET /submit - logs every parameter
+app.get('/submit', (req, res)=>{
+    console.log('--- GET Request Received ---');
+    console.log('Query Parameters: ', req.query);
+    Object.entries(req.query).forEach(([key, value]) => {
+        console.log(`${key}: ${value}`);
+    });
+    console.log('----------------------------');
+    res.send('<h2>GET request received!</h2><pre>' + JSON.stringify(req.query, null, 2) + '</pre>');
+});
+
+//POST /submit - logs body contents
+app.post('/submit', (req,res) =>{
+    console.log('--- POST Request Received ---');
+    console.log('Body')
+})
+
+app.listen(3030);
