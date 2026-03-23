b4fd9345d1b4c9fe8c31e1371e48a39deb3a7e92
Lebron <alebron1290@gmail.com>
2026-03-23 11:33:06 -0400

    WorkinOnQuiz

---
+++ routes/quiz.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+//Work goes here
+
+
+
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,17 @@ router.get('/', (req, res)=>{
     res.send('User List');
 });
 
+router.get('/list', (req,res)=>{
+    res.send
+})
+
 router.get('/new', (req, res)=>{
     res.send('User New Form');
 
 
+});
+router.get('/:id', (req,res)=>{
+    res.send (`Getting User Data: ${req.params.id}`);
 });
 
 module.exports = router;
